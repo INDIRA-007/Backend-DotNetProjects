acd8721e65f397ca0783762e873df4e3bf93b84c
indiraarokiyamary13 <indiraarokiyamary13@gmail.com>
2025-08-03 13:38:31 +0530

    commiting CsharpMethod

---
+++ CsharpMethods/TestProject/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double total =0;
+double minimumSpend = 30.00;
+
+double[] items = { 15.97, 3.50, 12.25, 22.99, 10.98 };
+double[] discounts = { 0.30,0.00, 0.10,0.20, 0.50};
+
+
+
+for(int i=0; i< items.Length; i++)
+{
+    total += getDiscounterPrice(i);
+}
+
+total -= totalMeetsMinimum() ? 5.00 : 0.00;
+
+Console.WriteLine($"Total: ${formatDecimal(total)}");
+
+double getDiscounterPrice(int itemIndex)
+{
+    return items[itemIndex] * (1 - discounts[itemIndex]);
+    //return result;
+}
+
+bool totalMeetsMinimum()
+{
+    return total >= minimumSpend;
+}
+
+string formatDecimal(double input)
+{
+    return input.ToString().Substring(0, 5);
+}
