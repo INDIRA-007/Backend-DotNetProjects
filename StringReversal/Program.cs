fa4bcd3494fc30d4109ba9d73586a2200132871d
indiraarokiyamary13 <indiraarokiyamary13@gmail.com>
2025-08-03 13:51:15 +0530

    committing stringreversal

---
+++ StringReversal/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get the input from user
+            Console.Write("Enter the string to be reversed: ");
+            string input = Console.ReadLine();
+            //call the function to be excuted- function declaration
+            Console.WriteLine(Reversal(input));
+
+        }
+
+        //function definition
+
+        static string Reversal(string input)
+        {
+            string reversedString = string.Empty;
+
+            char[] ch = input.ToCharArray();
+            for(int chlen = ch.Length-1; chlen >=0; chlen--)
+            {
+                reversedString = reversedString + ch[chlen];
+            }
+
+            return reversedString;
+        }
+
+    }
+}
