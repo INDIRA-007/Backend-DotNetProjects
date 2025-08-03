dbe8422d361f40aa6ecb6ba3b3aa8ddccdc76ee6
indiraarokiyamary13 <indiraarokiyamary13@gmail.com>
2025-08-04 01:55:20 +0530

    code on exceptionhandling

---
+++ Exceptionhandling/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+double float1 = 3000.0;
+double float2 = 0.0;
+int number1 = 3000;
+int number2 = 0;
+
+//Console.WriteLine(float1 / float2);
+//Console.WriteLine(number1 / number2);
+Console.WriteLine("Exit program");
+
+try
+{
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
+}
+catch
+{
+    Console.WriteLine("An exception has been caught");
+}
+
+// inputValues is used to store numeric values entered by a user
+string[] inputValues = new string[] { "three", "9999999999", "0", "2" };
+
+foreach (string inputValue in inputValues)
+{
+    int numValue = 0;
+    try
+    {
+        numValue = int.Parse(inputValue);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid readResult. Please enter a valid number.");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("The number you entered is too large or too small.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
