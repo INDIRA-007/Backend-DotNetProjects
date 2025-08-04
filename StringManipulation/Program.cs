c46426292975d4eb1935d23b359deb04d6696099
indiraarokiyamary13 <indiraarokiyamary13@gmail.com>
2025-08-04 22:21:44 +0530

    Commiting latest code

---
+++ StringManipulation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Dot Net Tricks Skills Challend";
+
+            string[] arr = str.Split(' ');
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("{0}",arr[i]);
+            }
+
+            string str1 = String.Concat(str, ", You should try");
+            Console.WriteLine(str1);
+
+            int index = str.IndexOf("Skills");
+            Console.WriteLine(index);
+
+            string newStr = str.Replace("Challend", "Challenges");
+            Console.WriteLine(newStr);
+
+            string lower = str.ToLower();
+            Console.WriteLine(lower);
+
+            string upper = str.ToUpper();
+            Console.WriteLine(upper);
+
+            string substring = str.Substring(0,25);
+            Console.WriteLine(substring);
+
+            //Console.ReadKey();
+
+            //immutable - System.String
+            //mutable - System.Text.StringBuilder
+
+            string newString = "Hellow World";
+            newString.Replace("World", "Dot Net developer");  //immutable string
+            Console.WriteLine(newString);
+
+            StringBuilder newStringBuilder = new StringBuilder("Hello World!");
+            newStringBuilder.Replace("World", ".Net developers");
+            Console.WriteLine(newStringBuilder);
+        }
+    }
+}
---
+++ StringManipulation/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StringManipulation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringManipulation")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ebd8a68a-4b25-4125-8aae-0a2785d3a0e7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
