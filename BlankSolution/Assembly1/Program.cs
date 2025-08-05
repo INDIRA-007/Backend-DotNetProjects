145ba8b8f332b9d539cea396b50ee9fdd79170b9
indiraarokiyamary13 <indiraarokiyamary13@gmail.com>
2025-08-05 13:47:50 +0530

    commit latest project

---
+++ BlankSolution/Assembly1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly1
+{
+    public class Assembly1BaseClass
+    {
+        private string privateVariable = "private";
+        protected string protectedVariable = "protected";
+        internal string internalVariable = "internal";
+        protected internal string protectedInternalVariable = "protected internal";
+        public string publicVariable = "public";
+
+        public void TestAccess()
+        {
+            //accessible
+            Console.WriteLine(privateVariable);
+            Console.WriteLine(protectedVariable);
+            Console.WriteLine(internalVariable);
+            Console.WriteLine(protectedInternalVariable);
+            Console.WriteLine(publicVariable);
+        }
+
+    }
+    public class Assembly1DerivedClass : Assembly1BaseClass
+    {
+        public void TestAccessDerivedClass()
+        {
+            //Not accessible
+            //Console.WriteLine(privateVariable);
+            
+            //accessbile
+            Console.WriteLine(protectedVariable);
+            Console.WriteLine(internalVariable);
+            Console.WriteLine(protectedInternalVariable);
+            Console.WriteLine(publicVariable);
+        }
+    }
+
+    public class Assembly1OtherClass
+    {
+        public void TestAccessOtherClass()
+        {
+            // to call properties of base class to other class without deriving it 
+            // create the object of baseclass and call the properties/methods object.property
+            Assembly1BaseClass objBase = new Assembly1BaseClass();
+
+            //Not accessible
+            //Console.WriteLine(objBase.privateVariable);
+            //Console.WriteLine(objBase.protectedVariable);
+            
+            //accessible
+            Console.WriteLine(objBase.internalVariable);
+            Console.WriteLine(objBase.protectedInternalVariable);
+            Console.WriteLine(objBase.publicVariable);
+        }
+    }
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assembly1BaseClass objBase = new Assembly1BaseClass();
+            //objBase.TestAccess();
+
+            Assembly1DerivedClass objDerivedClass = new Assembly1DerivedClass();
+            objDerivedClass.TestAccessDerivedClass();
+
+            Console.WriteLine();
+
+            //to access method of base class from object of derived class
+            objDerivedClass.TestAccess();
+
+            Console.WriteLine();
+
+            Assembly1OtherClass objOtherClass = new Assembly1OtherClass();
+            objOtherClass.TestAccessOtherClass();
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ BlankSolution/Assembly2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Assembly1;
+namespace Assembly2
+{
+    public class Assembly2DerivedClass : Assembly1BaseClass
+    {
+        public void TestAccessInDerivedClass()
+        {
+            //Not accessible
+            //Console.WriteLine(privateVariable);
+            //Console.WriteLine(internalVariable);
+
+            //accessbile
+            Console.WriteLine(protectedVariable);
+            Console.WriteLine(protectedInternalVariable);
+            Console.WriteLine(publicVariable);
+        }
+    }
+
+    public class Assembly2OtherClass
+    {
+        public void TestAccessOtherClass()
+        {
+            Assembly1BaseClass objBase = new Assembly1BaseClass();
+
+            //Not accessible
+            //Console.WriteLine(objBase.privateVariable);
+            //Console.WriteLine(objBase.internalVariable);
+            //Console.WriteLine(objBase.protectedVariable);
+            //Console.WriteLine(objBase.protectedInternalVariable);
+
+            ////accessbile
+            Console.WriteLine(objBase.publicVariable);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly2DerivedClass objDerived = new Assembly2DerivedClass();
+            objDerived.TestAccessInDerivedClass();
+
+            Console.WriteLine();
+            
+            //to access parent class object
+            objDerived.TestAccess();
+
+            Console.WriteLine();
+
+            Assembly2OtherClass objOther = new Assembly2OtherClass();
+            objOther.TestAccessOtherClass();
+                 
+
+        }
+    }
+}
