498c0b9fa8f9e240a50b47280e62327471dfc37c
indiraarokiyamary13 <indiraarokiyamary13@gmail.com>
2025-08-23 10:17:53 +0530

    Read and Write in a text file

---
+++ RWTextFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RWTextFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string FilePath = @"C:\Users\LENOVO\Documents\DotNetprojects2025";
+            List<string> TextLines = new List<string>();
+            File.ReadAllLines(FilePath).ToList();
+
+            foreach (string line in TextLines)
+            {
+                Console.WriteLine(line);
+            }
+
+            TextLines.Add("Try to add new lines");
+            File.WriteAllLines(FilePath, TextLines);
+
+            Console.ReadLine();
+        }
+    }
+}
