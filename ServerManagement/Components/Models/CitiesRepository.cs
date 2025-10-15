238aa99da23fc68f58f293681535429ccc136bf4
indiraarokiyamary13 <indiraarokiyamary13@gmail.com>
2025-10-15 19:59:49 +0530

    feat: creating new DotNetCore With Blazor Application-Server management

---
+++ ServerManagement/Components/Models/CitiesRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace ServerManagement.Components.Models
+{
+    public class CitiesRepository
+    {
+        private static List<string> cities = new List<string>()
+        {
+            "Toronto",
+            "Halifax",
+            "Montreal",
+            "Calgary",
+            "Ottawa"
+        };
+
+        public static List<string> GetCities() => cities;
+    }
+}
