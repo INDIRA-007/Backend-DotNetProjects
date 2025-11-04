8993a370904a7ad7e37b38d82ba2a8bc06b6a512
indiraarokiyamary13 <indiraarokiyamary13@gmail.com>
2025-11-04 13:30:42 +0530

    Feat: DSA Program and Linear Search Array in C#

---
+++ DSAPrograms/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Arrays
+
+//Array Insertions & Deletions
+
+//int[] intArray = new int[8];
+//int length = 0;
+
+//Inserting at the End of the Array
+
+//for (int i = 0; i < 8; i++)
+//{
+//    intArray[length] = i + 1;
+//    length++;
+//}
+
+//intArray[length] = 8;
+//length++;
+
+
+//Inserting at the start of the Array
+
+//for (int i = 3; i > 0; i--)
+//{
+//    intArray[i + 1] = intArray[i];
+//}
+//intArray[0] = 20;
+//int value = 0;
+
+//Inserting at the anywhere of the Array
+//for (int i=4; i>=2; i--)
+//{
+//    intArray[i + 1] = intArray[i];
+
+//}
+//intArray[2] = 8;
+//int value = 10;
+
+
+
+int[] intArray = new int[9];
+int length = 0;
+
+//for(int i=0; i< 6; i++)
+//{
+//    intArray[length] = i;
+//    length++;
+//}
+////length--;
+
+//for(int i=1; i<length; i++)
+//{
+//    intArray[i - 1] = intArray[i];
+//}
+//length--;
+
+//for(int i=0; i< length; i++)
+//{
+//    Console.WriteLine(intArray[i]);
+//}
+
+//Deleting anywhere
+for (int i = 0; i < 6; i++)
+{
+    intArray[length] = i;
+    length++;
+}
+for(int i=3; i<length; i++)
+{
+    intArray[i - 1] = intArray[i];
+}
+length--;
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine(intArray[i]);
+}
---
+++ LinearSearchArray/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+//1. For loop
+//2. if check
+
+int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+
+bool LinearSearch(int[] array, int key)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        if (array[i] == key)
+            return true;
+    }
+    return false;
+}
+
+Console.WriteLine("Element found in Linear Search: ", LinearSearch(array, 5));
